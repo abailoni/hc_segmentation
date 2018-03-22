7cf4806f3f3c46b6123a9e1972308f02df4cf707
abailoni <bailoni.alberto@gmail.com>
2018-03-22 15:24:47 +0100

    Add skeleton of block-wise solver

---
+++ long_range_hc/postprocessing/long_range_aggl.py
@@ -0,0 +1,127 @@
+import numpy as np
+
+from inferno.utils.io_utils import yaml2dict
+from skunkworks.inference.simple import SimpleParallelLoader
+
+# TODO: upgrade this basic version with something more advanced
+# here the bad thing is that in the final agglomeration all boundaries between blocks are hard vertical/horizontal boundaries
+
+class BlockWiseSegmentationPipelineSolver(object):
+    def __init__(self,
+                 segmentation_pipeline,
+                 crop_padding=False,
+                 offsets=None,
+                 nb_parallel_blocks=1,
+                 nb_threads=8,
+                 num_workers=1):
+        """
+        :param nb_parallel_blocks: how many blocks are solved in parallel
+        :param nb_threads: nb threads used computations in every block
+        :param num_workers: used to load affinities from file (probably not needed, since there is no augmentation)
+        """
+        self.segmentation_pipeline = segmentation_pipeline
+        self.nb_offsets = len(offsets)
+
+        if offsets is not None:
+            assert len(offsets) == self.nb_offsets, "%i, %i" % (len(offsets), self.nb_offsets)
+        self.offsets = offsets
+
+        self.crop_padding = crop_padding
+
+        self.nb_parallel_blocks = nb_parallel_blocks
+        if nb_parallel_blocks != 1:
+            raise NotImplementedError()
+        # TODO: not necessary!
+        self.nb_threads = nb_threads
+        self.num_workers = num_workers
+
+
+    @classmethod
+    def from_config(cls, config, segmentation_pipeline):
+        config = yaml2dict(config)
+        crop_padding = config.get("crop_padding", False)
+        nb_threads = config.get("nb_threads", 8)
+        nb_parallel_blocks = config.get("nb_parallel_blocks", 1)
+        num_workers = config.get("num_workers", 1)
+        offsets = config.get("offsets", None)
+        if offsets is not None:
+            offsets = [tuple(off) for off in offsets]
+        return cls(segmentation_pipeline,
+                   crop_padding=crop_padding,
+                   nb_threads=nb_threads,
+                   nb_parallel_blocks=nb_parallel_blocks,
+                   num_workers=num_workers,
+                   offsets=offsets)
+
+
+    def __call__(self, dataset):
+        # build the output volume
+        shape_affs = dataset.volume.shape
+        assert shape_affs[0] == self.nb_offsets
+        assert len(shape_affs) == 4
+        shape_output = shape_affs[1:]
+
+        output = np.zeros(shape_output, dtype='uint64')
+        # loader
+        loader = SimpleParallelLoader(dataset, num_workers=self.num_workers)
+        # mask to count the number of times a pixel was infered
+
+        max_label = 0
+        while True:
+            # TODO: parallelize (take care of the max label...)
+            batches = loader.next_batch()
+            if not batches:
+                print("[*] All blocks were processed")
+                break
+
+            assert len(batches) == 1
+            assert len(batches[0]) == 2
+            index, input_ = batches[0]
+            print("[+] Processing block {} of {}.".format(index, len(dataset)))
+            # print("[*] Input-shape {}".format(input_.shape))
+
+            # get the slicings w.r.t. the current prediction and the output
+            local_slicing, global_slicing = self.get_slicings(dataset.base_sequence[index],
+                                                              input_.shape,
+                                                              dataset.padding)
+            # remove offset dim from slicing
+            global_slicing = global_slicing[1:]
+            local_slicing = local_slicing[1:]
+
+            print("Global slice: {}".format(global_slicing))
+            output_patch = self.segmentation_pipeline(input_)
+
+            # TODO: ADD CROP OF PADDING. We should predict with a padding and then crop!
+
+            # save predictions in the output
+            output[global_slicing] = output_patch[local_slicing] + max_label
+            max_label += output_patch.max() + 1
+
+        # crop padding from the outputs
+        # This is only used to crop the dataset in the end
+        crop = tuple(slice(pad[0], shape_output[i] - pad[1]) for i, pad in enumerate(dataset.padding[1:]))
+        out_crop = (slice(None),) + crop
+        output = output[out_crop]
+
+        # TODO: add final global agglomeration...?
+
+        # return the prediction
+        return output
+
+
+    def get_slicings(self, slicing, shape, padding):
+        # crop away the padding (we treat global as local padding) if specified
+        # this is generally not necessary if we use blending
+        if self.crop_padding:
+            # slicing w.r.t the current output
+            local_slicing = tuple(slice(pad[0], shape[i] - pad[1])
+                                  for i, pad in enumerate(padding))
+            # slicing w.r.t the global output
+            global_slicing = tuple(slice(slicing[i].start + pad[0],
+                                         slicing[i].stop - pad[1])
+                                   for i, pad in enumerate(padding))
+        # otherwise do not crop
+        else:
+            local_slicing = np.s_[:]
+            global_slicing = slicing
+        return local_slicing, global_slicing
